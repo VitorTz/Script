d81e8961303b83b8e8b929e62639a00f75151888
VitorTz <vitor.ftz@outlook.com>
2023-02-17 09:09:13 -0300

    Update

--- bash/arch_install.sh
+++ bash/arch_install.sh
@@ -7,14 +7,15 @@ git config --global user.name "VitorTz"
 git config --global user.email "vitor.ftz@outlook.com"
 
 sudo pacman -S --needed --noconfirm git base-devel
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -Y --gendb
-yay -Syu --devel
-yay -Y --devel --save
-yay
+yay -Syu --noconfirm
 cd
 
 yay -S --noconfirm spotify discord visual-studio-code-bin android-studio gimp firefox alacritty papirus-icon-theme
+
+sudo pacman -R firedragon firedragon-extension-plasma-integration octopi kate
+
--- bash/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Remove orphans, limpa cache do pacman e yay e remove arquios temporarios
-
-sudo pacman -R $(pacman -Qdt | awk '{print $1}') --noconfirm
-sudo pacman -Scc --noconfirm && yay -Scc --noconfirm
-rm -rf /mnt/HD/TMP/*
-
-echo Done!
